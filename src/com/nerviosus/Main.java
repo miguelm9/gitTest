22f0d41301090228e822410c98d69bcc83983aef
nerviosus <nerviosus@protonmail.com>
2018-12-22 11:48:08 +0100

    working project

--- src/com/nerviosus/Main.java
+++ src/com/nerviosus/Main.java
@@ -3,6 +3,6 @@ package com.nerviosus;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("THIS PROJECT DOES NOT WORK");
+        System.out.println("THIS PROJECT DOES WORK");
     }
 }
