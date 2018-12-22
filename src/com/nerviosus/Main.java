22a64a2173a64fd8b3cc00b0ea27134a8bfc7208
nerviosus <nerviosus@protonmail.com>
2018-12-22 11:39:31 +0100

    project not working commit

--- src/com/nerviosus/Main.java
+++ src/com/nerviosus/Main.java
@@ -3,7 +3,6 @@ package com.nerviosus;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("HELLO WORLD!");
-        System.out.println("THIS PROJECT WORKS");
+        System.out.println("THIS PROJECT DOES NOT WORK");
     }
 }
